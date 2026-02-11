aca7e3740d70ce2c51da540821a900feaacdcdce
GeTOM1 <thomasgeale@hotmail.co.uk>
2026-02-11 19:30:29 +0000

    Update satellite.py

--- satellite.py
+++ satellite.py
@@ -1,3 +1,4 @@
+import pygame
 import pgzrun
 from random import randint
 from time import time
@@ -16,19 +17,19 @@ end_time = 0
 number_of_satellite = 8
 
 def create_satellites():
-    global start_time
+    global start_time, satellites
     for count in range(0, number_of_satellite):
         satellite = Actor("satellite")
         satellite.pos = randint(40, WIDTH-40), randint(40, HEIGHT-40)
         
         satellites.append(satellite)
-        start_time = time()
+    start_time = time()
 
 
 def draw():
     global total_time
 
-    screen.blit("background", (0, 0))
+    screen.blit("galaxy", (0, 0))
     number = 1
     for satellite in satellites:
         screen.draw.text(str(number), (satellite.pos[0], satellite.pos[1] +20))
@@ -40,10 +41,10 @@ def draw():
 
     if next_satellite < number_of_satellite:
         total_time = time() - start_time
-        screen.draw.text(str(round(total_time, 1)), (10, 10), font_size = 30)
+        screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
 
     else:
-        screen.draw.text(str(round(total_time, 1)), (10, 10), font_size = 30)
+        screen.draw.text(str(round(total_time, 1)), (10, 10), fontsize = 30)
 
 def update():
     pass
