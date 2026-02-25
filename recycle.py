ef49d7b4ed8bc5cd8e8f31e9788bc82c4c690d71
GeTOM1 <thomasgeale@hotmail.co.uk>
2026-02-25 19:56:30 +0000

    recycle.py

--- recycle.py
+++ recycle.py
@@ -9,7 +9,7 @@ CENTRE_Y = HEIGHT /2
 CENTRE = (CENTRE_X, CENTRE_Y)
 FINAL_LEVEL = 6
 START_SPEED = 10
-ITEM = ["bag", "battery", "bottle", "chips"]
+ITEMS = ["bag", "battery", "bottle", "chips"]
 
 game_over = False
 game_complete = False
@@ -34,8 +34,30 @@ def update():
     if len(items) == 0
     items = make_items(current_level)
 
-def make_items(number_of_extra_itens)
-    items_to_create = get_options_to_create(number_of_extra_itens)
+def make_items(number_of_extra_items)
+    items_to_create = get_options_to_create(number_of_extra_items)
     new_items = create_items(items_to_create)
     layout_items(new_items)
     return new_items
+
+def get_options_to_create(number_of_extra_items):
+    items_to_create = ["paper"]
+    for i in range(0, number_of_extra_items):
+        random_option = random.choice(ITEMS)
+        items_to_create.append(random_option)
+    return items_to_create
+
+def create_items(items_to_create):
+    new_items = []
+    for option in items_to_create:
+        item =Actor(option + "img")
+        new_items.append(item)
+    return new_items
+
+def layout_items(items_to_layout):
+    number_of_gaps = len(items_to_layout) + 1
+    gap_size = WIDTH / number_of_gaps
+    random.shuffle(items_to_layout)
+    for index, item in enumerate(items_to_layout):
+        new_x_pos = (index +1) * gap_size
+        item.x = new_x_pos
