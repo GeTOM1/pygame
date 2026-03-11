8abf0183d1efc338de30ca303275f570eb6b661b
GeTOM1 <thomasgeale@hotmail.co.uk>
2026-03-11 19:06:58 +0000

    recycle.py

--- recycle.py
+++ recycle.py
@@ -31,10 +31,10 @@ def draw():
 
 def update():
     global items
-    if len(items) == 0
-    items = make_items(current_level)
+    if len(items) == 0:
+        items = make_items(current_level)
 
-def make_items(number_of_extra_items)
+def make_items(number_of_extra_items):
     items_to_create = get_options_to_create(number_of_extra_items)
     new_items = create_items(items_to_create)
     layout_items(new_items)
@@ -61,3 +61,44 @@ def layout_items(items_to_layout):
     for index, item in enumerate(items_to_layout):
         new_x_pos = (index +1) * gap_size
         item.x = new_x_pos
+
+def animate_items(items_to_animate):
+    global animations
+    for item in items_to_animate:
+        duration = START_SPEED - current_level
+        item.anchor = ("center", "bottom")
+        animations = animate(item, duration = duration, on_finished = handle_game_over, y = HEIGHT)
+        animations.append(animations)
+
+def handle_game_over():
+    global game_over
+    game_over = True
+
+def on_mouse_down(pos):
+    global items, current_level
+    for item in items:
+        if item.collidepoint(pos):
+            if "paper" in item.image:
+                handle_game_complete
+            else:
+                handle_game_over
+
+def handle_game_complete():
+    global current_level, items, animations, game_complete
+    stop_animations(animations)
+    if current_level == FINAL_LEVEL:
+        game_complete = True
+    else:
+        current_level += 1
+        items = []
+
+def stop_animations(animations_to_stop):
+    for animation in animations_to_stop:
+        if animation.running:
+            animation.stop()
+
+def display_message(heading_text, sub_heading_text):
+    screen.draw.text(heading_text, fontsize = 60, centre = CENTRE, color = "white")
+    screen.draw.text(sub_heading_text, fontsize = 30, centre = (CENTRE_X, CENTRE_Y +30), color = "white")
+
+pgzrun.go()
